1f698843234c8cc4cfbea35e1dcea18d1a243330
Arnav Bhandari <arnavbhandari2328@gmail.com>
2026-02-20 12:10:39 +0530

    fix api url for production

--- src/api/api.js
+++ src/api/api.js
@@ -1,40 +1,25 @@
-const mapProduct = (p) => ({
-  id: p.id,
-  name: p.product_name,
-  sku: p.product_id,
-  lowStock: p.low_stock_alert,
-  createdAt: p.created_at,
-});
-const API_URL = "https://qr-inventory.onrender.com/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
-/* PRODUCTS */
 export const getProducts = async () => {
   const res = await fetch(`${API_URL}/products`);
-  const data = await res.json();
-  return data.map(mapProduct);
+  return res.json();
 };
 
-/* LOCATIONS */
 export const getLocations = async () => {
   const res = await fetch(`${API_URL}/locations`);
   return res.json();
 };
 
-/* TRANSACTIONS */
 export const getTransactions = async () => {
   const res = await fetch(`${API_URL}/transactions`);
-  const data = await res.json();
-return data || [];
+  return res.json();
 };
 
 export const addTransaction = async (data) => {
   const res = await fetch(`${API_URL}/transactions`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
-
-  return res.json();   // ⭐ VERY IMPORTANT
+  return res.json();
 };
